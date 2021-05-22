889223b5c14e9fc0c4ef898c02609aa80c8cac5b
Shivanee Deepak Shinde <shivanee.shinde@oracle.com>
2021-05-22 13:54:33 +0530

    first commit

---
+++ src/main/java/com/menrva/healthapp/HealthappApplication.java
@@ -0,0 +1,13 @@
+package com.menrva.healthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthappApplication.class, args);
+	}
+
+}
