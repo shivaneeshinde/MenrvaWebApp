8ad309e9afe34e3303f84d3df173d7f0d253f475
Shivanee Deepak Shinde <shivanee.shinde@oracle.com>
2021-09-10 15:16:36 +0530

    react issue fix code pushed

--- src/main/java/com/menrva/healthapp/controller/RestController.java
+++ src/main/java/com/menrva/healthapp/controller/RestController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+
+@CrossOrigin(origins = "https://menrvahealthapp.herokuapp.com/")
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
